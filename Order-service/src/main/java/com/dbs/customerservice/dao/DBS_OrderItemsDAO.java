cda9c13d174f50115e56837a1764258961da80e1
surendarreddy-kolan <67551592+surendarreddy-kolan@users.noreply.github.com>
2020-06-30 01:17:30 +0530

    Order_And_OrderItems-Task
    Order_And_OrderItems-TAsk

--- Order-service/src/main/java/com/dbs/customerservice/dao/DBS_OrderItemsDAO.java
+++
@@ -1,8 +0,0 @@
-package com.dbs.customerservice.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.dbs.commons.entity.DBS_OrderItemsEntity;
-
-public interface DBS_OrderItemsDAO extends JpaRepository<DBS_OrderItemsEntity, Long>{
-}
